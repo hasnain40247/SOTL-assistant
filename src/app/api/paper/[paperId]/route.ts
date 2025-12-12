48d2915bf35fba34565b13c4097a53b2ac93c194
Hasnain Sikora <hnsikora@gmail.com>
2025-12-12 17:29:59 -0500

    commit: updated db

--- src/app/api/paper/[paperId]/route.ts
+++ src/app/api/paper/[paperId]/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest } from 'next'
 import pdfParse from "pdf-parse";
 import Papa from "papaparse";
 
